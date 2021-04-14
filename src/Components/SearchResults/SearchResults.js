5eb1abf005b02f76f02083c3db029aff33e7d389
kldyckman <71660710+kldyckman@users.noreply.github.com>
2021-04-14 16:36:31 -0400

    Pass Down Search Result and Render Result List

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,14 +1,16 @@
 import React from 'react';
-/*import TrackList from */
+import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
 
-export class SearchResults extends React.Component {
+class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/* Add a TrackList component */}
+                <TrackList tracks={this.props.searchResults} />
             </div>
         );
     }
 }
+
+export default SearchResults;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,13 +1,20 @@
 import React from 'react';
-/*import Track from */
+import Track from '../Track/Track';
 import './TrackList.css';
 
-export class TrackList extends React.Component {
+class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                {/* You will add a map method that renders a set of Track components */}
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track}
+                                    key={track.id} />
+                    })
+                }
             </div>
         );
     }
 }
+
+export default TrackList;
